5c54779f94a3c453f1b2c16fbf571a7763f99cb2
Kateryna <koljuchka93@gmail.com>
2024-01-05 03:55:28 +0200

    It works!!!

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace MatchingGame
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.label16 = new System.Windows.Forms.Label();
             this.label15 = new System.Windows.Forms.Label();
@@ -46,12 +47,13 @@ namespace MatchingGame
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.DarkOrchid;
             this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
             this.tableLayoutPanel1.ColumnCount = 4;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
@@ -76,13 +78,14 @@ namespace MatchingGame
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 4;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(532, 503);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(382, 353);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // label16
@@ -91,13 +94,13 @@ namespace MatchingGame
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label16.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label16.Location = new System.Drawing.Point(401, 377);
+            this.label16.Location = new System.Drawing.Point(290, 263);
             this.label16.Name = "label16";
-            this.label16.Size = new System.Drawing.Size(126, 124);
+            this.label16.Size = new System.Drawing.Size(87, 88);
             this.label16.TabIndex = 15;
             this.label16.Text = "G";
             this.label16.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label16.Click += new System.EventHandler(this.Form1_Click);
+            this.label16.Click += new System.EventHandler(this.Label_Click);
             // 
             // label15
             // 
@@ -105,13 +108,13 @@ namespace MatchingGame
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label15.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label15.Location = new System.Drawing.Point(269, 377);
+            this.label15.Location = new System.Drawing.Point(195, 263);
             this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(124, 124);
+            this.label15.Size = new System.Drawing.Size(87, 88);
             this.label15.TabIndex = 14;
             this.label15.Text = "G";
             this.label15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label15.Click += new System.EventHandler(this.Form1_Click);
+            this.label15.Click += new System.EventHandler(this.Label_Click);
             // 
             // label14
             // 
@@ -119,13 +122,13 @@ namespace MatchingGame
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label14.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label14.Location = new System.Drawing.Point(137, 377);
+            this.label14.Location = new System.Drawing.Point(100, 263);
             this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(124, 124);
+            this.label14.Size = new System.Drawing.Size(87, 88);
             this.label14.TabIndex = 13;
             this.label14.Text = "G";
             this.label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label14.Click += new System.EventHandler(this.Form1_Click);
+            this.label14.Click += new System.EventHandler(this.Label_Click);
             // 
             // label13
             // 
@@ -133,13 +136,13 @@ namespace MatchingGame
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label13.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label13.Location = new System.Drawing.Point(5, 377);
+            this.label13.Location = new System.Drawing.Point(5, 263);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(124, 124);
+            this.label13.Size = new System.Drawing.Size(87, 88);
             this.label13.TabIndex = 12;
             this.label13.Text = "G";
             this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label13.Click += new System.EventHandler(this.Form1_Click);
+            this.label13.Click += new System.EventHandler(this.Label_Click);
             // 
             // label12
             // 
@@ -147,13 +150,13 @@ namespace MatchingGame
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label12.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label12.Location = new System.Drawing.Point(401, 252);
+            this.label12.Location = new System.Drawing.Point(290, 176);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(126, 123);
+            this.label12.Size = new System.Drawing.Size(87, 85);
             this.label12.TabIndex = 11;
             this.label12.Text = "G";
             this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label12.Click += new System.EventHandler(this.Form1_Click);
+            this.label12.Click += new System.EventHandler(this.Label_Click);
             // 
             // label11
             // 
@@ -161,13 +164,13 @@ namespace MatchingGame
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label11.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label11.Location = new System.Drawing.Point(269, 252);
+            this.label11.Location = new System.Drawing.Point(195, 176);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(124, 123);
+            this.label11.Size = new System.Drawing.Size(87, 85);
             this.label11.TabIndex = 10;
             this.label11.Text = "G";
             this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label11.Click += new System.EventHandler(this.Form1_Click);
+            this.label11.Click += new System.EventHandler(this.Label_Click);
             // 
             // label10
             // 
@@ -175,13 +178,13 @@ namespace MatchingGame
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label10.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label10.Location = new System.Drawing.Point(137, 252);
+            this.label10.Location = new System.Drawing.Point(100, 176);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(124, 123);
+            this.label10.Size = new System.Drawing.Size(87, 85);
             this.label10.TabIndex = 9;
             this.label10.Text = "G";
             this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label10.Click += new System.EventHandler(this.Form1_Click);
+            this.label10.Click += new System.EventHandler(this.Label_Click);
             // 
             // label9
             // 
@@ -189,13 +192,13 @@ namespace MatchingGame
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label9.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label9.Location = new System.Drawing.Point(5, 252);
+            this.label9.Location = new System.Drawing.Point(5, 176);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(124, 123);
+            this.label9.Size = new System.Drawing.Size(87, 85);
             this.label9.TabIndex = 8;
             this.label9.Text = "G";
             this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label9.Click += new System.EventHandler(this.Form1_Click);
+            this.label9.Click += new System.EventHandler(this.Label_Click);
             // 
             // label8
             // 
@@ -203,13 +206,13 @@ namespace MatchingGame
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label8.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label8.Location = new System.Drawing.Point(401, 127);
+            this.label8.Location = new System.Drawing.Point(290, 89);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(126, 123);
+            this.label8.Size = new System.Drawing.Size(87, 85);
             this.label8.TabIndex = 7;
             this.label8.Text = "G";
             this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label8.Click += new System.EventHandler(this.Form1_Click);
+            this.label8.Click += new System.EventHandler(this.Label_Click);
             // 
             // label7
             // 
@@ -217,13 +220,13 @@ namespace MatchingGame
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label7.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label7.Location = new System.Drawing.Point(269, 127);
+            this.label7.Location = new System.Drawing.Point(195, 89);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(124, 123);
+            this.label7.Size = new System.Drawing.Size(87, 85);
             this.label7.TabIndex = 6;
             this.label7.Text = "G";
             this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label7.Click += new System.EventHandler(this.Form1_Click);
+            this.label7.Click += new System.EventHandler(this.Label_Click);
             // 
             // label6
             // 
@@ -231,13 +234,13 @@ namespace MatchingGame
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label6.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label6.Location = new System.Drawing.Point(137, 127);
+            this.label6.Location = new System.Drawing.Point(100, 89);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(124, 123);
+            this.label6.Size = new System.Drawing.Size(87, 85);
             this.label6.TabIndex = 5;
             this.label6.Text = "G";
             this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label6.Click += new System.EventHandler(this.Form1_Click);
+            this.label6.Click += new System.EventHandler(this.Label_Click);
             // 
             // label5
             // 
@@ -245,13 +248,13 @@ namespace MatchingGame
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label5.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label5.Location = new System.Drawing.Point(5, 127);
+            this.label5.Location = new System.Drawing.Point(5, 89);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(124, 123);
+            this.label5.Size = new System.Drawing.Size(87, 85);
             this.label5.TabIndex = 4;
             this.label5.Text = "G";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label5.Click += new System.EventHandler(this.Form1_Click);
+            this.label5.Click += new System.EventHandler(this.Label_Click);
             // 
             // label4
             // 
@@ -259,13 +262,13 @@ namespace MatchingGame
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label4.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label4.Location = new System.Drawing.Point(401, 2);
+            this.label4.Location = new System.Drawing.Point(290, 2);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(126, 123);
+            this.label4.Size = new System.Drawing.Size(87, 85);
             this.label4.TabIndex = 3;
             this.label4.Text = "G";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label4.Click += new System.EventHandler(this.Form1_Click);
+            this.label4.Click += new System.EventHandler(this.Label_Click);
             // 
             // label3
             // 
@@ -273,13 +276,13 @@ namespace MatchingGame
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label3.Location = new System.Drawing.Point(269, 2);
+            this.label3.Location = new System.Drawing.Point(195, 2);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(124, 123);
+            this.label3.Size = new System.Drawing.Size(87, 85);
             this.label3.TabIndex = 2;
             this.label3.Text = "G";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label3.Click += new System.EventHandler(this.Form1_Click);
+            this.label3.Click += new System.EventHandler(this.Label_Click);
             // 
             // label2
             // 
@@ -287,13 +290,13 @@ namespace MatchingGame
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.label2.Location = new System.Drawing.Point(137, 2);
+            this.label2.Location = new System.Drawing.Point(100, 2);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(124, 123);
+            this.label2.Size = new System.Drawing.Size(87, 85);
             this.label2.TabIndex = 1;
             this.label2.Text = "G";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label2.Click += new System.EventHandler(this.Form1_Click);
+            this.label2.Click += new System.EventHandler(this.Label_Click);
             // 
             // label1
             // 
@@ -303,21 +306,27 @@ namespace MatchingGame
             this.label1.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
             this.label1.Location = new System.Drawing.Point(5, 2);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(124, 123);
+            this.label1.Size = new System.Drawing.Size(87, 85);
             this.label1.TabIndex = 0;
             this.label1.Text = "G";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label1.Click += new System.EventHandler(this.Form1_Click);
+            this.label1.Click += new System.EventHandler(this.Label_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.TimerEvent);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(532, 503);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(382, 353);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "MatchingGame";
-            this.Click += new System.EventHandler(this.Form1_Click);
+            this.Click += new System.EventHandler(this.Label_Click);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -342,6 +351,7 @@ namespace MatchingGame
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
